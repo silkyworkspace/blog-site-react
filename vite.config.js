22144c9356330e7e517986d8c3236fa5e8a179aa
“sawa” <silkyworkspace@gmail.com>
2025-11-10 14:01:31 +0900

    configに,を追記

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/blog-site-react/"
+  base: "/blog-site-react/",
 })
